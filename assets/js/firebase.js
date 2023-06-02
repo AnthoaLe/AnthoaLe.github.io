8f9f69b385b9aa271a21a3a390454ed5699ee252
ShimamoKaze <anhle012800@gmail.com>
2023-06-01 17:39:06 -0700

    Some changes to the Firebase configurations

---
+++ assets/js/firebase.js
@@ -0,0 +1,21 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBRXByDBcDKweg59LtM6b3S8S4lXAC5rBc",
+//   authDomain: "anhoale-portfolio-landing-page.firebaseapp.com",
+//   projectId: "anhoale-portfolio-landing-page",
+//   storageBucket: "anhoale-portfolio-landing-page.appspot.com",
+//   messagingSenderId: "501584270406",
+//   appId: "1:501584270406:web:23ecd52759b6288d54a647",
+//   measurementId: "G-JFJ8ETWZYZ"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
